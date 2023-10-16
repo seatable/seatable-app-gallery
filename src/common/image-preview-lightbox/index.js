e5727eea3a09bfe048901d3292dc4024fc9ead13
Michael An <2331806369@qq.com>
2023-10-16 16:38:53 +0800

    update image preview light box version

--- src/common/image-preview-lightbox/index.js
+++ src/common/image-preview-lightbox/index.js
@@ -1,18 +1,7 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import { Button } from 'reactstrap';
 import Lightbox from '@seafile/react-image-lightbox';
-import ModalPortal from '../modal-portal';
-
 import '@seafile/react-image-lightbox/style.css';
-
-const zIndexes = {
-  IMAGE_PREVIEW_LIGHTBOX: 1051,
-};
-
-function checkDesktop() {
-  return window.innerWidth >= 768;
-}
 
 ImagePreviewerLightbox.propTypes = {
   imageItems: PropTypes.array.isRequired,
@@ -36,15 +25,6 @@ function ImagePreviewerLightbox(props) {
     console.log(error);
   }
 
-  const isDesktop = checkDesktop();
-  let overlay = {
-    zIndex: zIndexes.IMAGE_PREVIEW_LIGHTBOX
-  };
-  let reactModalStyle = { overlay };
-  if (!isDesktop) {
-    Object.assign(overlay, {backgroundColor: '#000'});
-  }
-
   const downloadImage = () => {
     let image = new Image();
     image.crossOrigin = 'anonymous';
@@ -63,28 +43,17 @@ function ImagePreviewerLightbox(props) {
   };
 
   return (
-    <Fragment>
-      <Lightbox
-        imageTitle={imageTitle}
-        mainSrc={imageItems[imageIndex]}
-        nextSrc={imageItems[(imageIndex + 1) % imageItemsLength]}
-        prevSrc={imageItems[(imageIndex + imageItemsLength - 1) % imageItemsLength]}
-        onCloseRequest={props.closeImagePopup}
-        onMovePrevRequest={props.moveToPrevImage}
-        onMoveNextRequest={props.moveToNextImage}
-        animationDisabled={true}
-        reactModalStyle={reactModalStyle}
-        enableZoom={isDesktop}
-      />
-      {!isDesktop && (
-        <ModalPortal>
-          <Button className="text-white position-fixed" style={{
-            zIndex: zIndexes.IMAGE_PREVIEW_LIGHTBOX + 1,
-            background: '#333', bottom: '20px', right:'20px'
-          }} onClick={downloadImage}><span className="dtable-font dtable-icon-download"></span></Button>
-        </ModalPortal>
-      )}
-    </Fragment>
+    <Lightbox
+      imageTitle={imageTitle}
+      mainSrc={imageItems[imageIndex]}
+      nextSrc={imageItems[(imageIndex + 1) % imageItemsLength]}
+      prevSrc={imageItems[(imageIndex + imageItemsLength - 1) % imageItemsLength]}
+      onCloseRequest={props.closeImagePopup}
+      onMovePrevRequest={props.moveToPrevImage}
+      onMoveNextRequest={props.moveToNextImage}
+      animationDisabled={true}
+      onClickDownload={downloadImage}
+    />
   );
 }
 
